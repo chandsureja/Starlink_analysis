028829897f7ee57a0e4a34286f5ad9e853c49f02
chandsureja <chandsureja989@gmail.com>
2023-06-26 16:40:00 +0200

    scalar test

--- testt.py
+++ testt.py
@@ -1,32 +1,10 @@
 from skyfield.api import EarthSatellite, load
 from scipy.optimize import differential_evolution
+from scipy import optimize
 import datetime
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-plt.figure(dpi=100)
-
-
-
 tle_lines = open('active_gp(21_06).txt').readlines()
-
-
-def get_position(unix_t):
-    
-    
-    t=load.timescale().utc(2023, 6, 21, 0, 30, unix_t)
-    #print(t)
-    #sat1 = EarthSatellite(line1_tle1, line2_tle1, ts=load.timescale())
-    #sat2 = EarthSatellite(line1_tle2, line2_tle2, ts=load.timescale())
-    position1 = sat1.at(t)
-    position2 = sat2.at(t)
-    return (position2 - position1).distance().km
-# Extract the TLE data
-
-
-# Create satellite objects
-
 def read_tle(tle_lines,i):
     if (i==1):
         start_index = 0
@@ -38,6 +16,47 @@ def read_tle(tle_lines,i):
     line1_tle = tle_lines[start_index+1].strip()
     line2_tle = tle_lines[start_index+2].strip()
     return line0_tle, line1_tle, line2_tle
+
+
+plt.figure(dpi=100)
+
+line0_tle1, line1_tle1, line2_tle1 = read_tle(tle_lines,1)
+
+
+sat1 = EarthSatellite(line1_tle1, line2_tle1,line0_tle1,ts=load.timescale())
+line0_tle2, line1_tle2, line2_tle2 = read_tle(tle_lines,2)
+
+sat2 = EarthSatellite(line1_tle2, line2_tle2,line0_tle2, ts=load.timescale())
+
+
+
+position1 = []
+position2 = []
+dist= []
+def get_position(unix_t):
+    
+    for time in unix_t:
+        t=load.timescale().utc(2023, 6, 21, 0, 30, time)
+    #print(t)
+    #sat1 = EarthSatellite(line1_tle1, line2_tle1, ts=load.timescale())
+    #sat2 = EarthSatellite(line1_tle2, line2_tle2, ts=load.timescale())
+       
+        position1.append(sat1.at(t))
+        position2.append(sat2.at(t))
+        dist_temp=(sat2.at(t) - sat1.at(t)).distance().km
+        dist.append(dist_temp)
+    return min(dist)
+
+rand_int3 = np.random.randint(50,1000, size=10 ,dtype='int64')
+test_res=get_position(rand_int3)
+
+print(test_res)
+
+# Extract the TLE data
+
+
+# Create satellite objects
+
 
     
 
@@ -56,22 +75,17 @@ def find_minimum(start_timestamp, end_timestamp):
     result = minimize_scalar(objective_function, bounds=(start_timestamp, end_timestamp), method='bounded')
 
     return result.fun, result.x
-line0_tle1, line1_tle1, line2_tle1 = read_tle(tle_lines,1)
 
-
-sat1 = EarthSatellite(line1_tle1, line2_tle1,line0_tle1,ts=load.timescale())
-line0_tle2, line1_tle2, line2_tle2 = read_tle(tle_lines,2)
-
-sat2 = EarthSatellite(line1_tle2, line2_tle2,line0_tle2, ts=load.timescale())
 r_min, r_max = -5, 5
 # define the bounds on the search
 bounds = [[r_min, r_max], [r_min, r_max]]
 # perform the differential evolution search
-result = differential_evolution(objective_function, bounds,args=(criteria,))
+result = differential_evolution(get_position, bounds)
 
 #min_val, t = find_minimum(0,86400)
-print(line1_tle2)
-t=load.timescale().utc(2023, 6, 21, 0, 30, t)
+print(result)
+print(get_position(7496.12550951232))
+#t=load.timescale().utc(2023, 6, 21, 0, 30, t)
 #print(t.utc_strftime())
 t1 = np.array([0])
 pos= np.array([0])
@@ -80,15 +94,18 @@ pos= np.array([0])
     pos_temp=get_position(t)
     pos = np.append(pos, pos_temp)
 '''
+#print(optimize.rosen(rand_int3))
 
-print(result)
+#print(result)
 plt.plot(t1,pos)
 plt.show()
-print(min_val)
+
+
+'''print(min_val)
 if(min_val < 5):
     print(min_val,t)
     print(line0_tle2)
-'''for i in range(len(tle_lines)+1):
+for i in range(len(tle_lines)+1):
     
     
     line0_tle2, line1_tle2, line2_tle2 = read_tle(tle_lines,i+1)
